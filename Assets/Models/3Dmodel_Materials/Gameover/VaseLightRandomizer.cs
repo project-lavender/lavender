906934830bb587206b14652576bc1682d68748ea
K2 <55353405+18ZK2@users.noreply.github.com>
2023-11-17 15:03:10 +0900

    ゲームオーバー画面のUI機能追加

---
+++ Assets/Models/3Dmodel_Materials/Gameover/VaseLightRandomizer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using static Unity.Mathematics.math;
+
+public class VaseLightRandomizer : MonoBehaviour
+{
+    [SerializeField] float amp = 0.5f, freq = 1f, c = 0.01f;
+    float t = 0f;
+    Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        light.intensity = amp * abs(noise.snoise(float2(0f, t * freq))) + c;
+    }
+}
--- Assets/Scenes/GameOver/GameOverUI.cs
+++ Assets/Scenes/GameOver/GameOverUI.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using UnityEngine.UI;
+using Unity.Mathematics;
+using static Unity.Mathematics.math;
 
-public class TitleLogoParams : MonoBehaviour
+public class GameOverUI : MonoBehaviour
 {
-    public float Grad = 15f;
-    [SerializeField] Image bc = null;
-
-    Material bcMat;
-
-
+    [SerializeField] Image blood;
+    [SerializeField] float amp = 1f, freq = 0.5f;
+    [SerializeField] Color bloodColor,ccolor;
+    float t = 0f;
     public void MoveScene(int i)
     {
         SceneManager.LoadScene(i);
@@ -25,14 +25,15 @@ public class TitleLogoParams : MonoBehaviour
         Application.Quit();
 #endif
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        bcMat = bc.material;
-    }
 
     private void Update()
     {
-        bcMat.SetFloat("_Grad", Grad);
+        t += Time.deltaTime;
+        blood.color = bloodColor * amp * abs(noise.snoise(float2(0f, t * freq))) + ccolor;
     }
+
+
+
+
+
 }
