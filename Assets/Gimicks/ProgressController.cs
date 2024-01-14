00c3d82aa7191133e15c9f0801271a1fb23327cd
DESKTOP-QKL64P9\shing <hebimeta3236@gmail.com>
2024-01-14 12:48:10 +0900

    プログレスコントローラー修正

--- Assets/Gimicks/ProgressController.cs
+++ Assets/Gimicks/ProgressController.cs
@@ -67,7 +67,7 @@ public class ProgressController : MonoBehaviour
 
                 
                 break;
-            case 300:
+            case 320:
                 //ストレッチャーとダイナモは明かりを消す
                 foreach (Gimicks g in allgimicks)
                 {
